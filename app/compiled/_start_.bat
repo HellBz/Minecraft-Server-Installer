45919bed71cf27aa7b930e79fa9bdaec76f0dbc1
HellBz <stefan-koegler@freenet.de>
2024-10-05 03:33:32 +0200

    Change Build Gradle and Start-File Add SH-Start File

--- app/compiled/_start_.bat
+++ app/compiled/_start_.bat
@@ -1,6 +1,5 @@
 
         @echo off
-        echo Build Date: Sat Oct 05 03:23:20 CEST 2024
         echo Project Name: Minecraft-Server-Installer
         java -jar Minecraft-Server-Installer.jar
         pause
---
+++ app/compiled/_start_.sh
@@ -0,0 +1,6 @@
+
+        #!/bin/bash
+        echo "Project Name: Minecraft-Server-Installer"
+        java -jar Minecraft-Server-Installer.jar
+        read -p "Press any key to continue..."
+        
