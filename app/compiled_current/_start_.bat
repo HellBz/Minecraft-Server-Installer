c50d2f67c8ac8959c99d61010227accda82b2a0c
HellBz <stefan-koegler@freenet.de>
2024-10-05 03:17:35 +0200

    Change Output, to not save all Files everytime

---
+++ app/compiled_current/_start_.bat
@@ -0,0 +1,7 @@
+
+        @echo off
+        echo Build Date: Sat Oct 05 03:15:38 CEST 2024
+        echo Project Name: Minecraft-Server-Installer
+        java -jar Minecraft-Server-Installer.jar
+        pause
+        
